38c135a024e43da0bfa938c32d0c10127b0e5a81
Lagat100 <weldonkiprop1@gmail.com>
2024-01-08 11:32:34 -0800

    is of kind of

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""returns True if the object is an instance of, or if the object is an instance of a class that inherited from, the specified class"""
+
+def is_kind_of_class(obj, a_class):
+    """
+
+    return True if the object is an instance of, or if the object is an instance of a class that inherited from, the specified class
+
+    """
+    return type(obj) == a_class
