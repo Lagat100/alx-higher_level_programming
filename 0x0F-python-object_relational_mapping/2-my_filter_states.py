edfdb3e8c7abf9ffcc98d1a1ddeaa0edf2ec26cc
Lagat100 <weldonkiprop1@gmail.com>
2024-03-16 13:17:55 -0700

    fix task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
-import MySQLdb
-import sys
+"""lists all states from the database hbtn_0e_0_usa"""
 
+if __name__ == '__main__':
 
-def search_states_by_name(username, password, database, state_name):
+    import MySQLdb
+    import sys
+
     db = MySQLdb.connect(
-            host='localhost',
-            port=3306,
-            user=username,
-            passwd=password,
-            db=database)
+            host='localhost', port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3])
 
     cur = db.cursor()
-    sql_query = "SELECT * FROM states\
-            WHERE name = '{}' ORDER BY id ASC;".format(state_name)
-    cur.execute(sql_query)
+    x = sys.argv[4]
+    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id;"
+    cur.execute(query.format(x))
     rows = cur.fetchall()
     for row in rows:
         print(row)
