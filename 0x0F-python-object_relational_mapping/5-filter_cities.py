c8c38713fcc31c3dea1a268cc33e66b958396c55
Lagat100 <weldonkiprop1@gmail.com>
2024-03-16 15:34:12 -0700

    fix 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -14,10 +14,10 @@ if __name__ == '__main__':
 
     cur = db.cursor()
     state_name = sys.argv[4]
-    query = "SELECT cities.id, cities.name, states.name FROM cities \
-            LEFT JOIN states ON cities.state_id = states.id \
+    query = "SELECT cities.name FROM cities \
+            INNER JOIN states ON states.id = cities.state_id \
            WHERE BINARY states.name = %s ORDER BY cities.id ASC;"
-    cur.execute(query, (state_name,))
+    cur.execute(query, (state_name, ))
     rows = cur.fetchall()
     for row in rows:
         print(row)
