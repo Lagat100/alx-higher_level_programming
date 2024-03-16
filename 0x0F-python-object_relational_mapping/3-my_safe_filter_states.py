de512472d8321ea88da4527e5ab8466b96a7a2b2
Lagat100 <weldonkiprop1@gmail.com>
2024-03-16 13:30:32 -0700

    avoid sql injection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""lists all states from the database hbtn_0e_0_usa"""
+
+if __name__ == '__main__':
+
+    import MySQLdb
+    import sys
+
+    db = MySQLdb.connect(
+            host='localhost', port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3])
+
+    cur = db.cursor()
+    x = sys.argv[4]
+    query = "SELECT * FROM states WHERE BINARY name = %s ORDER BY id;"
+    cur.execute(query, (x))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
