511ab13aeccab8d28c5d23a8f55c476798a6e128
Lagat100 <weldonkiprop1@gmail.com>
2024-03-18 13:30:02 -0700

    10th task

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-"""A script that prints the first State
-objects from the database hbtn_0e_6_usa"""
+"""A script that prints the State passed as argument
+from the database hbtn_0e_6_usa"""
+
+
+from sys import argv
 
 
 if __name__ == "__main__":
@@ -9,14 +12,15 @@ if __name__ == "__main__":
     from model_state import Base, State
     from sqlalchemy import create_engine
     from sqlalchemy.orm import Session
+
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(sys.argv[1], sys.argv[2],
                                    sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
     session = Session(engine)
-    states =  session.query(State)\
-                    .filter(State.name.like('%a%'))\
-                    .order_by(State.id):
-    for state in states:
-        print("{}: {}".format(state.id, state.name))
+    state = session.query(State).filter(State.name == sys.argv[4]).first()
+    if state:
+        print("{}".format(state.id))
+    else:
+        print("Not found")
     session.close()
